d8669825d9588c982636e97f28ec5da1a248f00b
Guk <101082667+GukSense@users.noreply.github.com>
2023-03-09 11:43:30 +0900

    Initial commit

---
+++ src/main/java/com/lcomputerstudy/example/Lcomputerstudytwo1Application.java
@@ -0,0 +1,14 @@
+package com.lcomputerstudy.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lcomputerstudytwo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lcomputerstudytwo1Application.class, args);
+		System.out.println("System on");
+	}
+
+}
